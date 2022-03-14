4680ff4ab5fab20ee1b0113558ebf08a9ffc93f5
devrawr <devrawr@protonmail.com>
2022-03-14 22:48:57 +0100

    add comment, Tasks#33

--- src/main/kotlin/io/github/devrawr/tasks/Tasks.kt
+++ src/main/kotlin/io/github/devrawr/tasks/Tasks.kt
@@ -30,6 +30,12 @@ object Tasks
         if (this.plugin == null)
         {
             throw IllegalStateException("sync() may only be called from a bukkit context")
+            // I don't think there's a way to schedule tasks without halting the thread it's called from,
+            // therefore, there is no non-bukkit implementation for this.
+            // Bukkit, afaik, ticks every task in the main server loop, which makes it possible.
+            // unfortunately, because we wouldn't be able to (easily) override methods and tick our tasks,
+            // I don't think it's possible.
+            // If anyone has some kind of workaround, of course, feel free to open a pull request.
         }
 
         return BukkitSyncTaskScheduler
